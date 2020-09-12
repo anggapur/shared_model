7b7b82ec156b3db978db8c287a0886b8b5b9d353
Angga Purnajiwa <arimbawaangga@gmail.com>
2020-09-12 14:49:57 +0800

    RevenueCenterDevice model

---
+++ src/RevenueCenterDevice.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Poska;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class RevenueCenterDevice extends Model
+{
+    use SoftDeletes;
+
+    protected $table = "master_revenue_center_device";
+    protected $fillable = [        
+        "serial_number_device",
+        "revenue_center_id",
+        "information",
+        "created_at",
+        "updated_at",
+        "deleted_at"
+    ];
+}
