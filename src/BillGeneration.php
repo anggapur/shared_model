dc5bada35adb4d5d5da5a2f071f4b29c0af06ed3
Angga Purnajiwa <arimbawaangga@gmail.com>
2020-11-04 23:32:35 +0800

    Add Bill Generation Model

---
+++ src/BillGeneration.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Poska;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BillGeneration extends Model
+{
+    use SoftDeletes;
+
+    protected $table = "master_bill_generation";
+    protected $fillable = [
+        "revenue_center_id",
+        "restaurant_id",
+        "latest_bill_number",        
+        "latest_bill", 
+        "created_at",
+        "updated_at",
+        "deleted_at"
+    ];
+
+    public function restaurant()
+    {
+        return $this->belongsTo('Poska\Restaurant','restaurant_id');
+    }
+
+    public function revenue_center()
+    {
+        return $this->belongsTo('Poska\RevenueCenter','revenue_center_id');
+    }
+}
